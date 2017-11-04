b62c66ac3b01862c9baddecf0e80076d1c0d17d3
Sawepeter <sawepeter6@gmail.com>
2017-11-04 22:24:11 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,29 @@
+package com.sawepeter.mathgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //button object
+      final   Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent I;
+        I = new Intent(this, GameActivity.class);
+        startActivity(I);
+
+    }
+}
+
